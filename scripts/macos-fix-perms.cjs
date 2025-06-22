71153c52768cbeb4045b8b6db41136be7684cc5a
bea4dev <bea0224@outlook.jp>
2025-06-22 17:53:23 +0900

    bump version 0.0.11 -> 0.0.12

--- scripts/macos-fix-perms.cjs
+++
@@ -1,7 +0,0 @@
-const { chmodSync } = require('fs');
-const { execSync } = require('child_process');
-
-const bin = `${__dirname}/../server/tyml-lsp-server-macos-aarch64`;
-
-chmodSync(bin, 0o755);
-try { execSync(`xattr -d com.apple.quarantine "${bin}"`); } catch (_) {}
