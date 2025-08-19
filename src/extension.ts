5744e05c52e6d983581a65d81d8b2d2149eb4a3b
bea4dev <bea0224@outlook.jp>
2025-08-19 20:52:44 +0900

    impl lsp support for tyml-mock

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export async function activate(context: vscode.ExtensionContext) {
   }
 
   const serverOptions: ServerOptions = {
-    run:   { command: serverExe, transport: TransportKind.stdio },
+    run: { command: serverExe, transport: TransportKind.stdio },
     debug: { command: serverExe, transport: TransportKind.stdio }
   };
 
@@ -24,6 +24,35 @@ export async function activate(context: vscode.ExtensionContext) {
     documentSelector: [{ scheme: 'file', language: 'tyml' }],
     initializationOptions: {}
   };
+
+  var mockExe: string;
+  if (process.platform === "win32") {
+    mockExe = context.asAbsolutePath(path.join('mock', "tyml-mock-windows-x86_64.exe"));
+  } else if (process.platform === "darwin") {
+    mockExe = context.asAbsolutePath(path.join('mock', "tyml-mock-macos-aarch64"));
+  } else {
+    mockExe = context.asAbsolutePath(path.join('mock', "tyml-mock-linux-x86_64"));
+  }
+
+  let terminal: vscode.Terminal | null = null;
+  context.subscriptions.push(
+    vscode.commands.registerCommand("tyml.mock.serve", async (filePath: string, interfaceName: string) => {
+      if (terminal) {
+        terminal.dispose();
+      }
+
+      terminal = vscode.window.createTerminal("TYML Mock Server");
+      terminal.show();
+      terminal.sendText(`${mockExe} serve ${filePath} ${interfaceName}`);
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("tyml.mock.send", async (filePath: string, interfaceName: string, functionName: string) => {
+      const terminal = vscode.window.createTerminal("TYML Mock Client");
+      terminal.show();
+      terminal.sendText(`${mockExe} send ${filePath} ${interfaceName} ${functionName}`);
+    })
+  );
 
   client = new LanguageClient('tyml_lsp', 'TYML LSP Server', serverOptions, clientOptions);
   client.start();
